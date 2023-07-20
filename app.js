446640aa301ac93970738051f3cdab2b513b4a8c
Hassam Ahmed <121582888+hassamahmedsiddiqui@users.noreply.github.com>
2023-07-20 16:55:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,268 @@
+let luxuryWatches = {
+    watches:{
+        casioWatches:{
+            casioEdifice :{
+                name: 'Casio Edific',
+                brand : 'Casio',
+                price : '20.4$',
+                img: "images/casio/Casio-edifice.webp",
+            }
+            ,
+            casioEqb : {
+                name: 'Casio EQB',
+                brand : 'Casio',
+                price : '18.4$',
+                img: "images/casio/Casio EQB.png",
+            },
+            casioGD : {
+                name: 'Casio GD-4',
+                brand : 'Casio',
+                price : '33.7$',
+                img: "images/casio/Casio GD-4.webp" ,
+            },
+            casioGmw : {
+                name: 'Casio GMW',
+                brand : 'Casio',
+                price : '23.2$',
+                img: "images/casio/Casio Gmw.webp"  ,
+            },
+            casioGshock : {
+                name: 'Casio G-Shock',
+                brand : 'Casio',
+                price : '88.7$',
+                img: "images/casio/Casio G-shock.png"  ,
+            },
+            casioMtp : {
+                name: 'Casio MTP',
+                brand : 'Casio',
+                price : '42.7$',
+                img: "images/casio/Casio MTP.webp"  ,
+            },
+            casioVintage : {
+                name: 'Casio Vintage',
+                brand : 'Casio',
+                price : '92.4$',
+                img: "images/casio/Casio Vintage.webp" ,
+            },
+        },
+    }, 
+     citi : {
+        citizen : {
+            citizen88x : {
+                name: 'Citizen  NJ0151-88X',
+                brand : 'Citizen',
+                price : '67.4$',
+                img: "images/citizen/Citizen  NJ0151-88X.png" ,
+            },
+            citizen87A : {
+                name: 'Citizen BI5000-87A',
+                brand : 'Citizen',
+                price : '48.6$',
+                img: "images/citizen/Citizen BI5000-87A.jpg",
+            },
+            citizen81P : {
+                name: 'Citizen BI5006-81P',
+                brand : 'Citizen',
+                price : '37.3$',
+                img: "images/citizen/Citizen BI5006-81P.jpg",
+            },
+            citizen01L : {
+                name: 'Citizen BN0166-01L',
+                brand : 'Citizen',
+                price : '43.7$',
+                img: "images/citizen/Citizen BN0166-01L.png",
+            },
+            citizen59x : {
+                name: 'Citizen CA4542-59X',
+                brand : 'Citizen',
+                price : '31.2$',
+                img: "images/citizen/Citizen CA4542-59X.png",
+            },
+            citizen56x : {
+                name: 'Citizen NJ015',
+                brand : 'Citizen',
+                price : '71.2$',
+                img: "images/citizen/Citizen NJ0150-56X_.png",
+            },
+            citizen88mm : {
+                name: 'Citizen NJ01-88M',
+                brand : 'Citizen',
+                price : '33.2$',
+                img: "images/citizen/Citizen NJ0151-88M.png",
+            },
+        },
+    },
+        iwat : {
+        iWatches : {
+            appleRoyal :{
+                name : 'Apple Royal',
+                brand : 'Apple',
+                price : '18.7$',
+                img : "images/iwatches/Apple Royal.webp"
+            },
+            appleSeries8 :{
+                name : 'Apple Series8',
+                brand : 'Apple',
+                price : '102$',
+                img : "images/iwatches/Apple Royal.webp"
+            },
+            appleSeries8 :{
+                name : 'Apple Series8',
+                brand : 'Apple',
+                price : '102$',
+                img : "images/iwatches/Apple series 8.png",
+            },
+            appleSeries7 :{
+                name : 'Apple Series7',
+                brand : 'Apple',
+                price : '73$',
+                img : "images/iwatches/Apple series7.png",
+            },
+            appleSeries840mm :{
+                name : 'Apple S-40mm',
+                brand : 'Apple',
+                price : '54$',
+                img : "images/iwatches/Apple series8 40mm.png",
+            },
+            appleUltra :{
+                name : 'Apple Ultra-49mm',
+                brand : 'Apple',
+                price : '111$',
+                img : "images/iwatches/Apple Ultra-49mm.webp",
+            },
+        },
+    },
+        qua: {
+        quartz : {
+            quartz80 : {
+                name : '80Diamonds',
+                brand : 'Quartz',
+                price : '136.3$',
+                img : "images/quatz/Quartz 80diamonds.png" ,
+            },
+            quartzGeneva : {
+                name : 'Quartz Geneva',
+                brand : 'Quartz',
+                price : '36.3$',
+                img : "images/quatz/Quartz geneva.webp" ,
+            },
+            quartzLe : {
+                name : 'Loyal Engima',
+                brand : 'Quartz',
+                price : '36.3$',
+                img : "images/quatz/Quartz loyalEngima.webp" ,
+            },
+            
+            quartzSwiss : {
+                name : 'Quartz Swiss',
+                brand : 'Quartz',
+                price : '63.3$',
+                img : "images/quatz/Quartz swiss.png" ,
+            },
+            quartzTissot : {
+                name : 'Quartz Tissot',
+                brand : 'Quartz',
+                price : '83.3$',
+                img : "images/quatz/Quartz Tissot.png" ,
+            },
+            quartzTocattta : {
+                name : 'Quartz Toccata',
+                brand : 'Quartz',
+                price : '92.4$',
+                img : "images/quatz/Quartz TOCCATA.png",
+            },
+        },
+    },
+        seve : {
+        seveston : { 
+            sevestonAero : {
+                name : 'Seveston Aero',
+                brand : 'Seveston',
+                price : '17.4$',
+                img : "images/seveston watches/Seveston Aero.webp",
+            },
+            sevestonBlossom : {
+                name : 'Seveston Blossom',
+                brand : 'Seveston',
+                price : '14.9$',
+                img : "images/seveston watches/Seveston Blossom.jfif" ,
+            },
+            sevestonBolt : {
+                name : 'Seveston Bolt',
+                brand : 'Seveston',
+                price : '54.9$',
+                img : "images/seveston watches/Seveston Bolt.jfif" ,
+            },
+            sevestonEtheral : {
+                name : 'Seveston Ethereal',
+                brand : 'Seveston',
+                price : '37.9$',
+                img : "images/seveston watches/Seveston Ethereal.webp" ,
+            },
+            sevestonTagwar : {
+                name : 'Seveston Tagwar',
+                brand : 'Seveston',
+                price : '15.9$',
+                img : "images/seveston watches/Seveston latest.webp" ,
+            },
+            sevestonLeather : {
+                name : 'Seveston Signature',
+                brand : 'Seveston',
+                price : '45.3$',
+                img : "images/seveston watches/Seveston LeatherStrap.webp" ,
+
+            }
+        },
+    },
+}
+
+
+
+
+var mainPage = document.getElementById("mainPage");
+ allItems =() => {
+    mainPage.innerHTML = ""
+ for(var key in luxuryWatches){
+     for(var watches in luxuryWatches[key])
+    for (var item in luxuryWatches[key][watches]){
+        console.log(item)
+        // console.log(luxuryWatches[key][watches][item].img)
+    mainPage.innerHTML += `
+    <div class="card">
+    <img src= "${luxuryWatches[key][watches][item].img}" height="220" width="220px" alt="">
+    <h4>${luxuryWatches[key][watches][item].name}</h4>
+    <h6> Brand: <span>${luxuryWatches[key][watches][item].brand}</span></h6>
+    <h6> Price :  <span>${luxuryWatches[key][watches][item].price}</span></h6>
+    <button class="Btn">
+    </button>
+    
+</div>`
+
+}
+ }}
+allItems()
+
+
+function select (categories){
+    mainPage.innerHTML = "";
+    for(var key in luxuryWatches[categories]){
+    // console.log(key)
+    for(var watch in luxuryWatches[categories][key]){
+        console.log(watch)
+        // console.log(luxuryWatches[categories][key].name)
+        mainPage.innerHTML += `
+        <div class="card">
+        <img src= "${luxuryWatches[categories][key][watch].img}" height="220" width="220px" alt="">
+        <h4>${luxuryWatches[categories][key][watch].name}</h4>
+        <h6> Brand: <span>${luxuryWatches[categories][key][watch].brand}</span></h6>
+        <h6> Price :  <span>${luxuryWatches[categories][key][watch].price}</span></h6>
+        <button class="Btn">
+        </button>
+        
+    </div>`
+
+
+    }
+
+}
+}
